524c30d957ee38b502fcc8a3dcd24bc6dedcc74f
john davis <davis@doom.lan>
2024-10-12 15:36:20 -0400

    ignore safe area

--- hwsp2/ContentView.swift
+++ hwsp2/ContentView.swift
@@ -10,11 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            // This avoids the top and bottom where the rounded corners are located.
+            // This is considered the "safe area"
             Color(red:1, green: 0.8, blue: 0.0)  // gold
-                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-                .cornerRadius(20)
+//                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+//                .cornerRadius(20)
             Text("Your content goes here")
         }
+        .ignoresSafeArea(.all)
     }
 }
 
