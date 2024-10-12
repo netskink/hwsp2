4fdeeea1f88f5f57832bc4aa0a35bc42bfa386e7
john davis <davis@doom.lan>
2024-10-12 14:11:45 -0400

    Initial Commit

---
+++ hwsp2/hwsp2App.swift
@@ -0,0 +1,17 @@
+//
+//  hwsp2App.swift
+//  hwsp2
+//
+//  Created by john davis on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct hwsp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
