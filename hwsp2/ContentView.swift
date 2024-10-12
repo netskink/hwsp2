7d147481883ce5ed0bfb5f1b34c4952e9d9fc7cd
john davis <davis@doom.lan>
2024-10-12 15:41:28 -0400

    foregound color

--- hwsp2/ContentView.swift
+++ hwsp2/ContentView.swift
@@ -10,14 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            // This avoids the top and bottom where the rounded corners are located.
-            // This is considered the "safe area"
-            Color(red:1, green: 0.8, blue: 0.0)  // gold
-//                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-//                .cornerRadius(20)
-            Text("Your content goes here")
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+            Text("Your content")
+                .foregroundStyle(.yellow)
         }
-        .ignoresSafeArea(.all)
     }
 }
 
