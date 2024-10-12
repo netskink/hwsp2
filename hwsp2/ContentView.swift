dfe67f25513c565c6e71de1e67af3d53b0c586ed
john davis <davis@doom.lan>
2024-10-12 16:56:38 -0400

    lots of buttons with images

--- hwsp2/ContentView.swift
+++ hwsp2/ContentView.swift
@@ -13,9 +13,41 @@ struct ContentView: View {
         Image("US")
         // This one won't be read by voiceover
         Image(decorative:"US")
+        // voice over will read out the filename
         Image(systemName: "flag.checkered.2.crossed")
             .foregroundStyle(.red)
             .font(.largeTitle)
+        
+        Button {
+            print("button was tapped")
+        } label: {
+            Image(systemName: "flag")
+        }
+
+        Button("edit", systemImage: "pencil", action: executeDelete)
+
+        Button {
+            print("button was tapped")
+        } label: {
+            HStack {
+                Image(systemName: "flag")
+                Text("US")
+            }
+        }
+
+        Button {
+            print("button was tapped")
+        } label: {
+            // better
+            Label("equivalent", systemImage: "flag")
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.red)
+                .cornerRadius(20)
+        }
+
+        
+        
     }
     
     func executeDelete() {
