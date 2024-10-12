b2f27696e8283951f0a8192b8adda9ef22d793ba
john davis <davis@doom.lan>
2024-10-12 15:54:01 -0400

    radialGradient

--- hwsp2/ContentView.swift
+++ hwsp2/ContentView.swift
@@ -9,23 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        //LinearGradient(colors: [.red, .blue], startPoint: .top, endPoint: .bottom)
-        
-        // This has the colors doing the actual gradient in a narrow range.
-//        LinearGradient(
-//            stops: [
-//                Gradient.Stop(color: .red, location: 0.45),
-//                Gradient.Stop(color: .blue, location: 0.55)
-//            ],
-//            startPoint: .top, endPoint: .bottom)
-
-        // Alternative of above
-        LinearGradient(
-            stops: [
-                .init(color: .red, location: 0.45),
-                .init(color: .blue, location: 0.55)
-            ],
-            startPoint: .top, endPoint: .bottom)
+        RadialGradient(colors: [.red, .blue], center: .center, startRadius: 20, endRadius: 200)
     }
 }
 
