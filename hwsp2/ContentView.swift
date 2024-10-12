d90aba9de9d11709fc5edb11bc6228fb850e3bcc
john davis <davis@doom.lan>
2024-10-12 15:50:40 -0400

    gradient.stop

--- hwsp2/ContentView.swift
+++ hwsp2/ContentView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LinearGradient(colors: [.red, .blue], startPoint: .top, endPoint: .bottom)
+        //LinearGradient(colors: [.red, .blue], startPoint: .top, endPoint: .bottom)
+        
+        // This has the colors doing the actual gradient in a narrow range.
+        LinearGradient(
+            stops: [
+                Gradient.Stop(color: .red, location: 0.45),
+                Gradient.Stop(color: .blue, location: 0.55)
+            ],
+            startPoint: .top, endPoint: .bottom)
     }
 }
 
