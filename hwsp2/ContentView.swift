81a4691fb81f1a173ea74cd6682fd69e30a5cd54
john davis <davis@doom.lan>
2024-10-12 15:52:35 -0400

    alternative to gradient.stop using .init

--- hwsp2/ContentView.swift
+++ hwsp2/ContentView.swift
@@ -12,10 +12,18 @@ struct ContentView: View {
         //LinearGradient(colors: [.red, .blue], startPoint: .top, endPoint: .bottom)
         
         // This has the colors doing the actual gradient in a narrow range.
+//        LinearGradient(
+//            stops: [
+//                Gradient.Stop(color: .red, location: 0.45),
+//                Gradient.Stop(color: .blue, location: 0.55)
+//            ],
+//            startPoint: .top, endPoint: .bottom)
+
+        // Alternative of above
         LinearGradient(
             stops: [
-                Gradient.Stop(color: .red, location: 0.45),
-                Gradient.Stop(color: .blue, location: 0.55)
+                .init(color: .red, location: 0.45),
+                .init(color: .blue, location: 0.55)
             ],
             startPoint: .top, endPoint: .bottom)
     }
