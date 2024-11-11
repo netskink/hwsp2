9b68c62ba83153ccf402fd7d6fbcba7ddeddcec3
john davis <davis@doom.lan>
2024-11-11 15:50:34 +0700

    complete

--- hwsp2/ContentView.swift
+++ hwsp2/ContentView.swift
@@ -8,47 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State var  countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State var correctAnswer = Int.random(in: 0...2)
-
+    @State var score = 0
+    
     var body: some View {
         
         ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
             
-        VStack(spacing: 30) {
             VStack {
-                Text("Tap the flag of")
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
                     .foregroundStyle(.white)
-                    .font(.subheadline.weight(.heavy))
+                
+                
+                // current VStack(spacing: 15) code
+                
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.white)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .foregroundStyle(.white)
+                            .font(.largeTitle.weight(.semibold))
+                        
+                    } // vstack
+                    
+                    ForEach(0..<3) {
+                        number in
+                        Button {
+                            // flag was tapped
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 10)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.regularMaterial)
+                    .clipShape(.rect(cornerRadius: 20))
+                }  // vstack
+                
+                Spacer()
+                Spacer()
 
-                Text(countries[correctAnswer])
+                Text("Score: \(score)")
                     .foregroundStyle(.white)
-                    .font(.largeTitle.weight(.semibold))
-
-            } // vstack
-            
-            ForEach(0..<3) {
-                number in
-                Button {
-                    // flag was tapped
-                    flagTapped(number)
-                } label: {
-                    Image(countries[number])
-                        .clipShape(.capsule)
-                        .shadow(radius: 10)
-                }
-            }
-        }  // vstack
+                    .font(.title.bold())
+                Spacer()
+            }  // vstack
+            .padding()
         } // zstack
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score ???? ")
+            Text("Your score \(score) ")
         }
         
     } // var body: some view
@@ -56,12 +84,14 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct!"
+            score = score + 1
         } else {
             scoreTitle = "Wrong!"
+            score = score - 1
         }
         showingScore = true
     }  // flagTapped
-
+    
     func askQuestion() {
         countries = countries.shuffled()
         correctAnswer = Int.random(in: 0...2)
